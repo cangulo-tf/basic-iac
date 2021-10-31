78d255908164d96e6334f3cfc223032733e6b8de
Carlos Angulo <carlos.angulo.mascarell@outlook.com>
2021-10-31 21:27:17 +0100

    fix: improved TF output

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,13 @@
 output "apps" {
-  value = [for o in values(module.organization_apps) : tomap({
+  value = [for o in values(module.organization_apps) : {
     name     = o.org_name
     id       = o.org_id
     arn      = o.org_arn
     accounts = length(o.accounts) > 0 ? "${length(o.accounts)} accounts has been created" : "no accounts provided"
     envs     = length(o.envs) > 0 ? "${length(o.envs)} environments has been created" : "no envs provided"
-  })]
+  }]
+}
+
+output "resourgroup_name" {
+  value = aws_resourcegroups_group.main.name
 }
